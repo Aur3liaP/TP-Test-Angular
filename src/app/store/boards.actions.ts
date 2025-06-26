7f4419d5ce21d7e88118ffd4e1e77c54bbb84258
Pic Aurélia <pic.aurelia@gmail.com>
2025-06-26 15:13:43 +0200

    Add actions for Task+lists

--- src/app/store/boards.actions.ts
+++ src/app/store/boards.actions.ts
@@ -1,6 +1,7 @@
 import { createAction, props } from '@ngrx/store';
-import { Board } from './boards.models';
+import { Board, List, Task } from './boards.models';
 
+// -----------------------Actions pour les boards-----------------------
 export const loadBoards = createAction('[Boards] Load Boards');
 export const loadBoardsSuccess = createAction(
   '[Boards] Load Boards Success',
@@ -9,4 +10,45 @@ export const loadBoardsSuccess = createAction(
 export const selectBoard = createAction(
   '[Boards] Select Board',
   props<{ boardId: number }>()
+);
+
+
+// -----------------------Actions pour les tâches-----------------------
+export const addTask = createAction(
+  '[Tasks] Add Task',
+  props<{ boardId: number; listId: number; task: Omit<Task, 'id'> }>()
+);
+
+export const updateTask = createAction(
+  '[Tasks] Update Task',
+  props<{ boardId: number; listId: number; task: Task }>()
+);
+
+export const moveTask = createAction(
+  '[Tasks] Move Task',
+  props<{ 
+    boardId: number; 
+    sourceListId: number; 
+    targetListId: number; 
+    taskId: number; 
+    sourceIndex: number; 
+    targetIndex: number; 
+  }>()
+);
+
+export const reorderTask = createAction(
+  '[Tasks] Reorder Task',
+  props<{ 
+    boardId: number; 
+    listId: number; 
+    previousIndex: number; 
+    currentIndex: number; 
+  }>()
+);
+
+
+// -----------------------Actions pour les listes-----------------------
+export const updateList = createAction(
+  '[Lists] Update List',
+  props<{ boardId: number; list: List }>()
 );
